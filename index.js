1747df8784c678f1eb5d2aaad032029d2849516f
AhaD <abuahad35188@gmail.com>
2023-06-10 19:38:22 +0600

    sum bug fixed

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ app.post('/my-class',async(req,res)=>{
      const result = await myclassCollection.insertOne(item)
      res.send(result);
 })
-app.delete('/my-class/:id',verifyJWT,verifyAdmin, async(req,res)=>{
+app.delete('/my-class/:id', async(req,res)=>{
      const id = req.params.id;
      const query = {_id : new ObjectId(id)};
      const result = await myclassCollection.deleteOne(query);
